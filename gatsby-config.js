45420f17eabd61404c61c4e936bc054a6df43182
DavidDeprost <dadeprost@gmail.com>
2019-11-16 15:14:44 +0100

    Reproduce 'gatsby build' sass bug;
    On 'gatsby develop', the page will be green.
    On 'gatsby build && gatsby serve', it will just be white.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    'gatsby-plugin-sass',
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import vars from "./index.scss"
 
-export default () => <div>Hello world!</div>
+console.log('vars = ', vars);
+console.log('vars.green = ', vars.green);
+
+export default () =>
+  <div style={{
+      minHeight: '100vh',
+      maxWidth: '100vw',
+      background: vars.green
+  }}>
+      Hello world!
+  </div>
