b786f4f97d38a96cd153962a5ea2635da8674074
DavidDeprost <dadeprost@gmail.com>
2019-11-19 16:01:54 +0100

    Implement class component and onClicks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,80 @@ import React from 'react';
 import './index.scss';
 
 
-export default () =>
-  <div id='layout'>
-      <div id='left'>
-          <div className='header'>
-              <div className='icon'>
-                L
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      leftActive: true,
+      rightActive: true,
+    }
+  }
+
+  toggleLeft = () => {
+    this.setState({ leftActive: !this.state.leftActive });
+  }
+
+  toggleRight = () => {
+    this.setState({ rightActive: !this.state.rightActive });
+  }
+
+  render() {
+    let leftActive = this.state.leftActive ? 'leftActive' : 'leftPassive';
+    let rightActive = this.state.rightActive ? 'rightActive' : 'rightPassive';
+
+    return (
+      <div
+        id='layout'
+        className={`${leftActive} ${rightActive}`}
+      >
+          <div id='left' className={leftActive} >
+              <div className='header'>
+                  <div
+                    className='icon'
+                    onClick={this.toggleLeft}
+                  >
+                    L
+                  </div>
+                  <div className='title'>
+                    Left header
+                  </div>
               </div>
-              <div className='title'>
-                Left header
+              <div className='content'>
+                  Left content
               </div>
           </div>
-          <div className='content'>
-              Left content
+
+          <div id='main'>
+              <div className='header'>
+                  <div className='title'>
+                    Main header
+                  </div>
+              </div>
+              <div className='content'>
+                  Main content
+              </div>
+          </div>
+
+          <div id='right' className={rightActive} >
+              <div className='header'>
+                  <div
+                    className='icon'
+                    onClick={this.toggleRight}
+                  >
+                    R
+                  </div>
+                  <div className='title'>
+                    Right header
+                  </div>
+              </div>
+              <div className='content'>
+                  Right content
+              </div>
           </div>
       </div>
-      <div id='main'>
-          <div className='header'>
-              <div className='title'>
-                Main header
-              </div>
-          </div>
-          <div className='content'>
-              Main content
-          </div>
-      </div>
-      <div id='right'>
-          <div className='header'>
-              <div className='icon'>
-                R
-              </div>
-              <div className='title'>
-                Right header
-              </div>
-          </div>
-          <div className='content'>
-              Right content
-          </div>
-      </div>
-  </div>
+    );
+  }
+}
+
+export default Layout;
