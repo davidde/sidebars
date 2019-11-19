6c04ac97885762fd2db3acde0e5323b96cd18c22
DavidDeprost <dadeprost@gmail.com>
2019-11-19 14:03:23 +0100

    Experiment with grid layout for sidebars

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
-import React from "react"
-import vars from "./index.scss"
+import React from 'react';
+import './index.scss';
 
-console.log('vars = ', vars);
-console.log('vars.green = ', vars.green);
 
 export default () =>
-  <div style={{
-      minHeight: '100vh',
-      maxWidth: '100vw',
-      background: vars.green
-  }}>
-      Hello world!
+  <div id='layout'>
+      <div id='left'>
+          Leftside!
+      </div>
+      <div id='main'>
+          Main!
+      </div>
+      <div id='right'>
+          Rightside!
+      </div>
   </div>
