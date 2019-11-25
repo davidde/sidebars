23d6164e4eba0beb2a1c9c42024cfbd0effa5e6b
DavidDeprost <dadeprost@gmail.com>
2019-11-25 22:37:07 +0100

    Fix portrait sidebar activation

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,26 +12,6 @@ class Layout extends React.Component {
     }
   }
 
-  // componentDidMount() {
-  //   this.leftSidebar = document.querySelector('#left .sidebar');
-  //   if (this.leftSidebar.classList.contains('open')) {
-  //     this.leftSidebar.classList.add('flex-width');
-  //   }
-  //   this.leftSidebar.addEventListener('transitionend', this.snapToFlex);
-  // }
-
-  // componentWillUnmount() {
-  //   this.leftSidebar.removeEventListener('transitionend', this.snapToFlex);
-  // }
-
-  // snapToFlex = () => {
-  //   if (this.leftSidebar.classList.contains('open')) {
-  //     this.leftSidebar.classList.add('flex-width');
-  //   } else {
-  //     this.leftSidebar.classList.remove('flex-width');
-  //   }
-  // }
-
   toggleSidebar = (event) => {
     let key = `${event.currentTarget.parentNode.id}Open`;
     this.setState({ [key]: !this.state[key] });
@@ -49,16 +29,18 @@ class Layout extends React.Component {
                    onClick={this.toggleSidebar} >
                    &equiv;
               </div>
-              <div className='header'>
-                  <h3 className='title'>
-                    Left header
-                  </h3>
-              </div>
-              <div className='content'>
-                  <h3>Left content</h3>
-                  <p>
-                    Aenean ut felis finibus, aliquet mi a, feugiat felis. Donec porta, odio et vulputate laoreet, nibh odio iaculis mi, et ornare nulla orci vitae ligula. Sed mi velit, aliquam sit amet efficitur eget, scelerisque vel ligula. Aliquam finibus erat nec accumsan posuere. Vestibulum rhoncus, velit vitae volutpat vehicula, leo orci faucibus eros, at ornare nibh nunc nec mi. Donec porttitor ultricies mauris quis euismod. Praesent sem libero, venenatis ut ornare eget, volutpat tincidunt lacus. Pellentesque aliquam turpis et mauris consectetur, quis condimentum nunc dignissim. Cras lectus libero, pellentesque non malesuada at, condimentum nec ex. Nam sed accumsan enim. Donec eros massa, malesuada quis nulla elementum, imperdiet condimentum orci. Integer non velit et nulla vestibulum vestibulum. Proin vehicula tristique libero, eu tincidunt erat cursus ac. Ut malesuada ante ut est dictum, ornare varius arcu aliquet. Quisque vitae libero eget orci tristique aliquam id sit amet nunc.
-                  </p>
+              <div className={`sidebar ${leftOpen}`} >
+                  <div className='header'>
+                    <h3 className='title'>
+                      Left header
+                    </h3>
+                  </div>
+                  <div className='content'>
+                      <h3>Left content</h3>
+                      <p>
+                        Aenean ut felis finibus, aliquet mi a, feugiat felis. Donec porta, odio et vulputate laoreet, nibh odio iaculis mi, et ornare nulla orci vitae ligula. Sed mi velit, aliquam sit amet efficitur eget, scelerisque vel ligula. Aliquam finibus erat nec accumsan posuere. Vestibulum rhoncus, velit vitae volutpat vehicula, leo orci faucibus eros, at ornare nibh nunc nec mi. Donec porttitor ultricies mauris quis euismod. Praesent sem libero, venenatis ut ornare eget, volutpat tincidunt lacus. Pellentesque aliquam turpis et mauris consectetur, quis condimentum nunc dignissim. Cras lectus libero, pellentesque non malesuada at, condimentum nec ex. Nam sed accumsan enim. Donec eros massa, malesuada quis nulla elementum, imperdiet condimentum orci. Integer non velit et nulla vestibulum vestibulum. Proin vehicula tristique libero, eu tincidunt erat cursus ac. Ut malesuada ante ut est dictum, ornare varius arcu aliquet. Quisque vitae libero eget orci tristique aliquam id sit amet nunc.
+                      </p>
+                  </div>
               </div>
           </div>
 
@@ -95,16 +77,18 @@ class Layout extends React.Component {
                    onClick={this.toggleSidebar} >
                    &equiv;
               </div>
-              <div className='header'>
-                  <h3 className='title'>
-                    Right header
-                  </h3>
-              </div>
-              <div className='content'>
-                  <h3>Right content</h3><br/>
-                  <p>
-                    Mauris velit turpis, scelerisque at velit sed, porta varius tellus. Donec mollis faucibus arcu id luctus. Etiam sit amet sem orci. Integer accumsan enim id sem aliquam sollicitudin. Etiam sit amet lorem risus. Aliquam pellentesque vestibulum hendrerit. Pellentesque dui mauris, volutpat vel sapien vitae, iaculis venenatis odio. Donec vel metus et purus ullamcorper consequat. Mauris at ullamcorper quam, sed vehicula felis. Vestibulum fringilla, lacus sit amet finibus imperdiet, tellus massa pretium urna, non lacinia dui nibh ut enim. Nullam vestibulum bibendum purus convallis vehicula. Morbi tempor a ipsum mattis pellentesque. Nulla non libero vel enim accumsan luctus.
-                  </p>
+              <div className={`sidebar ${rightOpen}`} >
+                  <div className='header'>
+                    <h3 className='title'>
+                      Right header
+                    </h3>
+                  </div>
+                  <div className='content'>
+                      <h3>Right content</h3><br/>
+                      <p>
+                        Mauris velit turpis, scelerisque at velit sed, porta varius tellus. Donec mollis faucibus arcu id luctus. Etiam sit amet sem orci. Integer accumsan enim id sem aliquam sollicitudin. Etiam sit amet lorem risus. Aliquam pellentesque vestibulum hendrerit. Pellentesque dui mauris, volutpat vel sapien vitae, iaculis venenatis odio. Donec vel metus et purus ullamcorper consequat. Mauris at ullamcorper quam, sed vehicula felis. Vestibulum fringilla, lacus sit amet finibus imperdiet, tellus massa pretium urna, non lacinia dui nibh ut enim. Nullam vestibulum bibendum purus convallis vehicula. Morbi tempor a ipsum mattis pellentesque. Nulla non libero vel enim accumsan luctus.
+                      </p>
+                  </div>
               </div>
           </div>
 
